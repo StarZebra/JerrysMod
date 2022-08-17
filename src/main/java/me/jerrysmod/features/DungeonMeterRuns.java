c1b62af70bb533f6554e0618a441c73bbe85ee0c
StarZebra <Star@Zebra.gov>
2022-08-17 11:32:24 +0200

    Re-added non laggy DungeonMeterRuns

--- src/main/java/me/jerrysmod/features/DungeonMeterRuns.java
+++ src/main/java/me/jerrysmod/features/DungeonMeterRuns.java
@@ -1,6 +1,7 @@
 package me.jerrysmod.features;
 
 import me.jerrysmod.JerrysMod;
+import me.jerrysmod.events.TickEndEvent;
 import me.jerrysmod.utils.Utils;
 import net.minecraft.client.gui.inventory.GuiChest;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
@@ -13,53 +14,62 @@ import java.util.regex.Pattern;
 public class DungeonMeterRuns {
 	
 	private static String scoreLine;
+	private static short ticks = 0;
 	
 	@SubscribeEvent
 	public void onItemChange(ItemTooltipEvent event) {
 		if(!Utils.inSkyBlock) return;
 		
-		String name;
-		List<String> toolTip = event.toolTip;
-		String part1 = "Catacombs ";
-		String part2 = " RNG Meter";
-		String invName = Utils.getInventoryName();
-		if(invName.contains("(1/2)")){
-			name = invName.replace("(1/2) ", "");
-		}else if (invName.contains("(2/2)")){
-			name = invName.replace("(2/2) ", "");
-		}else{
-			name = invName;
-		}
-		
-		Pattern pattern = Pattern.compile(part1 + "[(\\w)]+" + part2);
-		Matcher matcher = pattern.matcher(name);
-		
-		boolean inGui = JerrysMod.mc.currentScreen instanceof GuiChest && matcher.matches();
-		
-		if(inGui){
-			String unformattedToolTip = Utils.removeFormatting(toolTip.toString());
-			if(unformattedToolTip.contains("Click to select!")){
-				int line = 0;
-				String[] tooltip = unformattedToolTip.split(", ");
-				for (String element: tooltip) {
-					if (element.startsWith("Dungeon Score: ")) {
-						scoreLine = toolTip.get(line);
-						break;
+		if(ticks % 20 == 0){
+			String name;
+			if(!Utils.getInventoryName().contains("RNG Meter")) return;
+			List<String> toolTip = event.toolTip;
+			String part1 = "Catacombs ";
+			String part2 = " RNG Meter";
+			String invName = Utils.getInventoryName();
+			if(invName.contains("(1/2)")){
+				name = invName.replace("(1/2) ", "");
+			}else if (invName.contains("(2/2)")){
+				name = invName.replace("(2/2) ", "");
+			}else{
+				name = invName;
+			}
+			
+			Pattern pattern = Pattern.compile(part1 + "[(\\w)]+" + part2);
+			Matcher matcher = pattern.matcher(name);
+			
+			boolean inGui = JerrysMod.mc.currentScreen instanceof GuiChest && matcher.matches();
+			
+			if(inGui){
+				String unformattedToolTip = Utils.removeFormatting(toolTip.toString());
+				if(unformattedToolTip.contains("Click to select!")){
+					int line = 0;
+					String[] tooltip = unformattedToolTip.split(", ");
+					for (String element: tooltip) {
+						if (element.startsWith("Dungeon Score: ")) {
+							scoreLine = toolTip.get(line);
+							break;
+						}
+						line++;
 					}
-					line++;
+					
+					String[] test = scoreLine.split("/");
+					String score = test[test.length-1];
+					String cleanScore = Utils.removeFormatting(score).replace(",", "");
+					float finalScore = Float.parseFloat(cleanScore);
+					float amountOfRuns = finalScore/300;
+					int ceilingRun = (int) Math.ceil(amountOfRuns);
+					String finalString = "§8(" + ceilingRun + "§8 S+ Runs)";
+					toolTip.set(line+1, finalString);
+					toolTip.add(line+2, "");
+					
 				}
-				
-				String[] test = scoreLine.split("/");
-				String score = test[test.length-1];
-				String cleanScore = Utils.removeFormatting(score).replace(",", "");
-				float finalScore = Float.parseFloat(cleanScore);
-				float amountOfRuns = finalScore/300;
-				int ceilingRun = (int) Math.ceil(amountOfRuns);
-				String finalString = "§8(" + ceilingRun + "§8 S+ Runs)";
-				toolTip.set(line+1, finalString);
-				toolTip.add(line+2, "");
-				
 			}
 		}
+	}
+	
+	@SubscribeEvent
+	public void onTick(TickEndEvent event){
+		ticks += 20;
 	}
 }
